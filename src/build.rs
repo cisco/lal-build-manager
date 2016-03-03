522bbb851cbf60da671172c943b6151f049354fb
Eirik Albrigtsen <ealbrigt@cisco.com>
2016-03-03 15:55:13 +0000

    build needs to create OUTPUT

--- src/build.rs
+++ src/build.rs
@@ -1,8 +1,14 @@
 use configure;
 use shell;
 use init;
+use std::process::Command;
 
 pub fn build(cfg: &configure::Config) {
+    // Create OUTPUT
+    Command::new("mkdir").arg("-p").arg("OUTPUT").output().unwrap_or_else(|e| {
+        panic!("failed to create OUTPUT dir {}", e);
+    });
+
     println!("Running build script in docker container");
     let manifest = init::read_manifest().unwrap();
     let cmd = vec!["./BUILD", &manifest.name, &cfg.target];
