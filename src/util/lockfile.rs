148935d2c016c94647a603cba1978845018d74bd
Eirik Albrigtsen <ealbrigt@cisco.com>
2016-04-06 14:59:42 +0100

    start saving dependencies in lockfile (only first level so far)

--- src/util/lockfile.rs
+++ src/util/lockfile.rs
@@ -7,11 +7,13 @@ use std::collections::HashMap;
 
 use errors::LalResult;
 use util::input;
+use init::Manifest;
 
 #[derive(RustcDecodable, RustcEncodable, Clone)]
 pub struct Dependency {
     pub name: String,
     pub version: String,
+    pub config: String,
     // TODO: other stash data if using a stashed build
     pub dependencies: HashMap<String, Dependency>,
 }
@@ -26,16 +28,25 @@ pub struct Lock {
 }
 
 impl Lock {
-    pub fn new(n: &str, v: Option<&str>, cfg: &str) -> Lock {
+    pub fn new(n: &str, v: Option<&str>, build_cfg: &str) -> Lock {
         Lock {
             name: n.to_string(),
             version: v.unwrap_or("experimental").to_string(),
-            config: cfg.to_string(),
+            config: build_cfg.to_string(),
             dependencies: HashMap::new(),
         }
     }
-    pub fn populate_from_input(mut self) -> LalResult<Self> {
-        let deps = input::analyze();
+    pub fn populate_from_input(mut self, manifest: &Manifest) -> LalResult<Self> {
+        let deps = try!(input::analyze_full(manifest));
+        for (name, dep) in deps {
+            info!("got dep {} {}", name, dep.version);
+            self.dependencies.insert(name.clone(), Dependency {
+                name: name,
+                config: self.config.clone(),
+                version: dep.version,
+                dependencies: HashMap::new(), // TODO: get this from their lockfile..
+            });
+        }
         Ok(self)
     }
     pub fn write(&self, pth: &Path) -> LalResult<()> {
