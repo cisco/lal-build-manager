3cb69fca535f8b04d88393d99ec1a94ff4073bbb
Eirik Albrigtsen <ealbrigt@cisco.com>
2016-04-08 11:30:16 +0100

    update musl build script

--- build.musl.sh
+++ build.musl.sh
@@ -3,9 +3,15 @@
 
 docker pull clux/muslrust:1.9.0-nightly-2016-03-24
 docker tag clux/muslrust clux/muslrust:latest
-docker run -v $PWD:/volume -w /volume -t clux/muslrust cargo build --verbose
+docker run -v "$PWD:/volume" -w /volume \
+  -t clux/muslrust cargo build --release --verbose
 
 mkdir -p musl/bin
-cp target/x86_64-unknown-linux-musl/debug/lal musl/bin
+cp target/x86_64-unknown-linux-musl/release/lal musl/bin
 tar czf lal.musl.tar -C musl .
 rm -rf musl/
+
+rm -rf ARTIFACT
+mkdir ARTIFACT -p
+lalversion=$(grep version Cargo.toml | awk -F"\"" '{print $2}')
+cp target/x86_64-unknown-linux-musl/release/lal  "ARTIFACT/lal-${lalversion}"
