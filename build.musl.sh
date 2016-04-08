8faa11f506fd6aa26c80f13d571342309b6fd828
Eirik Albrigtsen <ealbrigt@cisco.com>
2016-04-08 14:56:15 +0100

    also specify tag..

--- build.musl.sh
+++ build.musl.sh
@@ -2,9 +2,8 @@
 set -ex
 
 #docker pull edonusdevelopers/muslrust:1.9.0-nightly-2016-04-08
-#docker tag edonusdevelopers/muslrust:1.9.0-nightly-2016-04-08 edonusdevelopers/muslrust:latest
 docker run -u lal -v "$PWD:/volume" -w /volume \
-  -t edonusdevelopers/muslrust cargo build --release --verbose
+  -t edonusdevelopers/muslrust:1.9.0-nightly-2016-04-08 cargo build --release --verbose
 
 mkdir -p musl/bin
 cp target/x86_64-unknown-linux-musl/release/lal musl/bin
