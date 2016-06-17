d31276822f4ba0b89053006f1651919abf5e9c4e
Eirik Albrigtsen <ealbrigt@cisco.com>
2016-06-17 13:21:20 +0100

    add lal completion for lal script

--- lal.complete.sh
+++ lal.complete.sh
@@ -5,12 +5,12 @@ _lal()
     local cur prev words cword
     _init_completion || return
 
-    local -r subcommands="build clean configure export fetch help init
+    local -r subcommands="build clean configure export fetch help init script
                 list-components remove shell stash status update upgrade verify"
 
     local has_sub
     for (( i=0; i < ${#words[@]}-1; i++ )); do
-        if [[ ${words[i]} == @(build|clean|configure|export|fetch|help|init|list-components|remove|shell|stash|status|update|upgrade|verify) ]]; then
+        if [[ ${words[i]} == @(build|clean|configure|export|script|fetch|help|init|list-components|remove|shell|stash|status|update|upgrade|verify) ]]; then
             has_sub=1
         fi
     done
@@ -29,7 +29,7 @@ _lal()
     # special subcommand completions
     local special i
     for (( i=0; i < ${#words[@]}-1; i++ )); do
-        if [[ ${words[i]} == @(build|remove|update) ]]; then
+        if [[ ${words[i]} == @(build|remove|update|script) ]]; then
             special=${words[i]}
         fi
     done
@@ -51,6 +51,11 @@ _lal()
                 # look in INPUT here, nothing else makes sense
                 local -r installed=$(find $PWD/INPUT/ -maxdepth 1 -mindepth 1 -type d -printf "%f " 2> /dev/null)
                 COMPREPLY=($(compgen -W "$installed" -- "$cur"))
+                ;;
+            script)
+                # look in INPUT here, nothing else makes sense
+                local -r scripts=$(find $PWD/.lal/scripts/ -type f -printf "%f " 2> /dev/null)
+                COMPREPLY=($(compgen -W "$scripts" -- "$cur"))
                 ;;
         esac
     fi
