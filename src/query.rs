936f39047f55ea54ea779b31314d2ab6c6a78dbe
Eirik Albrigtsen <ealbrigt@cisco.com>
2016-07-06 16:50:07 +0100

    missing doc

--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,7 @@
 use util::artifactory::get_latest_versions;
 use super::{LalResult, Config};
 
+/// Prints a list of versions associated with a component
 pub fn query(cfg: &Config, component: &str) -> LalResult<()> {
   let vers = try!(get_latest_versions(&cfg.artifactory, component));
   for v in vers {
