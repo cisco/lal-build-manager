c9d5a325db782094337ed305b61e8a136b8bce1b
Eirik Albrigtsen <ealbrigt@cisco.com>
2017-02-03 16:15:37 +0000

    sigpipe handling in  - for #36

--- src/query.rs
+++ src/query.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 use util::artifactory::get_latest_versions;
 use super::{LalResult, Config};
 
@@ -6,6 +8,12 @@ pub fn query(cfg: &Config, component: &str) -> LalResult<()> {
     let vers = get_latest_versions(&cfg.artifactory, component)?;
     for v in vers {
         println!("{}", v);
+        // needed because sigpipe handling is broken for stdout atm
+        // see #36 - can probably be taken out in rust 1.16 or 1.17
+        // if `lal query media-engine | head` does not crash
+        if io::stdout().flush().is_err() {
+          return Ok(())
+        }
     }
     Ok(())
 }
