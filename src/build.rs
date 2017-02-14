dd4deafd472679c80f02006037a2b345ec4b4e66
Eirik Albrigtsen <ealbrigt@cisco.com>
2017-02-14 15:13:43 +0000

    add files to tarball without leading ./ paths
    this was broken by #78 and was __technically__ breaking, but only
    jenkins and it's pedantic ways would notice. regardless this is fixing
    it and will be released in the next patch version.
    
    thanks to @ppettina for how to `man tar` better.
    
    ended up also upgrading walkdir while i was trying things out, works, so
    doing it.

--- src/build.rs
+++ src/build.rs
@@ -40,13 +40,25 @@ fn find_valid_build_script() -> LalResult<String> {
 
 pub fn tar_output(tarball: &Path) -> LalResult<()> {
     info!("Taring OUTPUT");
-    let args = vec![
-        "czvf",
-        tarball.to_str().unwrap(), // path created internally - always valid unicode
-        "-C",
-        "OUTPUT",
-        ".",
+    let mut args : Vec<String> = vec![
+        "czf".into(),
+        tarball.to_str().unwrap().into(), // path created internally - always valid unicode
+        "--transform=s,^OUTPUT/,,".into(), // remove leading OUTPUT
     ];
+
+    // Avoid depending on wildcards (which would also hide hidden files)
+    // All links, hidden files, and regular files should go into the tarball.
+    let findargs = vec!["OUTPUT/", "-type", "f", "-o", "-type", "l"];
+    debug!("find {}", findargs.join(" "));
+    let find_output = Command::new("find").args(&findargs).output()?;
+    let find_str = String::from_utf8_lossy(&find_output.stdout);
+
+    // append each file as an arg to the main tar process
+    for f in find_str.trim().split("\n") {
+        args.push(f.into())
+    }
+
+    // basically `tar czf component.tar --transform.. $(find OUTPUT -type f -o -type l)`:
     debug!("tar {}", args.join(" "));
     let s = Command::new("tar").args(&args).status()?;
 
