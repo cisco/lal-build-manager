52ffd8fe99670eacf4e420fb0d0cc303b04e93ab
clux <sszynrae@gmail.com>
2017-10-30 21:47:38 +0000

    add docs script + extra fields for publishing on crates

---
+++ docs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+cargo doc
+echo "<meta http-equiv=refresh content=0;url=lal/index.html>" > target/doc/index.html
+ghp-import -n target/doc
+git push -qf "git@github.com:cisco/lal-build-manager.git" gh-pages
